29bf416849f0713213d48d841c1baa3011bc85b4
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-22 14:40:49 +0200

    Add unfinished models for logging events

--- exptracker/models.py
+++ exptracker/models.py
@@ -1,3 +1,5 @@
+from enum import Enum
+
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
@@ -9,7 +11,7 @@ class User(AbstractUser):
     Attributes
     ----------
     unspent_points : int
-        The number of points that the player can still distribute amongst his
+        The number of stars that the player can still distribute amongst his
         or her characters.
 
     """
@@ -24,11 +26,11 @@ class Character(models.Model):
     Attributes
     ----------
     points : int
-        The number points allocated to this character. From this we can
+        The number stars allocated to this character. From this we can
         calculate the character's level and the banners used for character
         progression. This should be set accordingly when adding a character.
-        For instance, a fresh level 6 character has 48 points. Four times eight
-        points for the first four levels and 16 points to progress from level 5
+        For instance, a fresh level 6 character has 48 stars. Four times eight
+        stars for the first four levels and 16 stars to progress from level 5
         to level 6.
 
     """
@@ -39,3 +41,31 @@ class Character(models.Model):
     points = models.PositiveIntegerField()
 
     # TODO: Attributes to retrieve a character's level and banner progression
+
+
+class LogType(Enum):
+    CHARACTER_ADDED = "CHARACTER_ADDED"
+    CHARACTER_DELETED = "CHARACTER_DELETED"
+    # The value for this event can be negative when stars are explicitely
+    # removed instead of being spent on a character, i.e. after having added
+    # too many or when stars are used to buy inspiration.
+    POINTS_MODIFIED = "POINTS_MODIFIED"
+    # Points spent on a character.
+    POINTS_SPENT = "POINTS_SPENT"
+
+
+class LogEntry(models.Model):
+    """
+    A log storing all changes made by a player.
+
+    This is mostly useful for reference.
+
+    """
+
+    user = models.ForeignKey(User, on_delete=models.DO_NOTHING, related_name="logs")
+    character = models.ForeignKey(
+        User, null=True, on_delete=models.DO_NOTHING, related_name="logs"
+    )
+
+    # TODO: Type
+    # TODO: Value
