64dcc071de9f27bc1b44f6b39e68b7e61873df6b
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-22 14:22:41 +0200

    Add a model for player characters

--- exptracker/models.py
+++ exptracker/models.py
@@ -16,3 +16,26 @@ class User(AbstractUser):
 
     unspent_points = models.PositiveIntegerField(default=0)
 
+
+class Character(models.Model):
+    """
+    A player's character.
+
+    Attributes
+    ----------
+    points : int
+        The number points allocated to this character. From this we can
+        calculate the character's level and the banners used for character
+        progression. This should be set accordingly when adding a character.
+        For instance, a fresh level 6 character has 48 points. Four times eight
+        points for the first four levels and 16 points to progress from level 5
+        to level 6.
+
+    """
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="characters")
+
+    name = models.CharField(max_length=255)
+    points = models.PositiveIntegerField()
+
+    # TODO: Attributes to retrieve a character's level and banner progression
