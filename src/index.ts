458f68e31f0e320e9aa6fa366dd84e134f964a94
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-24 18:39:19 +0200

    Add vue-router

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,18 @@
-import './styles/app.scss';
+import Vue from 'vue'
+import VueRouter from 'vue-router'
 
-import Overview from './components/overview.vue';
+import './styles/app.scss';
+import Router from './components/router.vue';
+import Overview from './components/routes/overview.vue';
 
 if (document.getElementById('app')) {
-  new Overview({ el: '#app' });
+  Vue.use(VueRouter);
+
+  const router = new VueRouter({
+    routes: [
+      { path: '*', component: Overview }
+    ]
+  });
+
+  new Router({ router }).$mount('#app');
 }
