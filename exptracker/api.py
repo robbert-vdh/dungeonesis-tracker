3e0b1d798caa691ccf4a802484920c73d4a4693a
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-25 17:44:47 +0200

    Add an API method for spending stars

--- exptracker/api.py
+++ exptracker/api.py
@@ -1,11 +1,16 @@
-from rest_framework import viewsets, permissions, serializers
+from django.db import transaction
+from django.db.models import F
+from rest_framework import permissions, serializers, viewsets
+from rest_framework.exceptions import APIException
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 from .models import Character
 
 # TODO: An API for adding and spending stars to and from the star pool
 
 
-class CharacterSelializer(serializers.ModelSerializer):
+class CharacterSerializer(serializers.ModelSerializer):
     class Meta:
         model = Character
         fields = ("id", "name", "stars")
@@ -15,8 +20,18 @@ class CharacterSelializer(serializers.ModelSerializer):
         read_only_fields = ("id", "stars")
 
 
+class StarRequestSerializer(serializers.Serializer):
+    """
+    A serializer that only stores a single integer, used for spending and
+    modifying a player's stars.
+
+    """
+
+    stars = serializers.IntegerField()
+
+
 class CharacterViewSet(viewsets.ModelViewSet):
-    serializer_class = CharacterSelializer
+    serializer_class = CharacterSerializer
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
@@ -24,3 +39,31 @@ class CharacterViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+    @action(detail=True, methods=["post"], name="Allocate stars from pool")
+    def spend(self, request, pk):
+        """Spend stars from the pool on this character."""
+
+        serializer = StarRequestSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        stars = serializer.validated_data["stars"]
+        if stars <= 0:
+            raise APIException(
+                "The number of stars spent must be positive and non-zero."
+            )
+
+        character = self.get_object()
+        with transaction.atomic():
+            if stars > request.user.unspent_stars:
+                raise APIException("You do not have enough stars.")
+
+            character.stars = F("stars") + stars
+            request.user.unspent_stars = F("unspent_stars") - stars
+
+            character.save()
+            request.user.save()
+
+            # TODO: Write to the log
+
+        return Response({"spent_stars": stars})
