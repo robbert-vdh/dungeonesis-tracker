b3ae9bb0d67e333dbfece824c6ed3a67edb8c467
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-26 17:48:05 +0200

    Allow adding characters when you do not have any
    This is not supposed to be some kind of CTF challenge.

--- src/components/pages/overview-page.vue
+++ src/components/pages/overview-page.vue
@@ -5,17 +5,23 @@
     <!-- TODO: Add a button for renaming a character to the character detail page -->
 
     <!-- TODO: Move this list and the status button to a separate component so we can use it in a dropdown -->
-    <div v-if="characters.length > 0" class="list-group">
-      <router-link v-for="character in characters"
-                   :key="character.id"
-                   :to="{ name: 'detail', params: { id: character.id } }"
-                   class="list-group-item list-group-item-action d-flex
-                          justify-content-between align-items-center">
-        {{ character.name }}
-        <span class="badge badge-primary badge-pill">
-          {{ calculateProgress(character.stars) }}
-        </span>
-      </router-link>
+    <div class="list-group">
+      <template v-if="characters.length > 0">
+        <router-link v-for="character in characters"
+                     :key="character.id"
+                     :to="{ name: 'detail', params: { id: character.id } }"
+                     class="list-group-item list-group-item-action d-flex
+                           justify-content-between align-items-center">
+          {{ character.name }}
+          <span class="badge badge-primary badge-pill">
+            {{ calculateProgress(character.stars) }}
+          </span>
+        </router-link>
+      </template>
+
+      <li v-else class="list-group-item disabled">
+        You do not have any characters yet.
+      </li>
 
       <a href="#"
          class="list-group-item list-group-item-action list-group-item-light
@@ -23,12 +29,6 @@
         <i class="icon-plus mr-2" aria-hidden="true"></i>Add a character
       </a>
     </div>
-
-    <ul v-else class="list-group">
-      <li class="list-group-item disabled">
-        You do not have any characters yet.
-      </li>
-    </ul>
   </div>
 </template>
 
