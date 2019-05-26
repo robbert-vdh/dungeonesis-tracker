b1658645d00159b445402a1edb9e7cb366d57760
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-26 11:45:17 +0200

    Add a TODO regarding missing tests

--- exptracker/api/__init__.py
+++ exptracker/api/__init__.py
@@ -1,2 +1,6 @@
 from . import characters
 from . import user
+
+# TODO: Add tests for the API methods. Tests are not very important here since
+#       the API is so permissive, but it's still pretty easy to break things in
+#       subtle ways without static type checking.
