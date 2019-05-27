5df729edcdc91bebb0a4abde9fc64c9162075d03
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-27 11:54:10 +0200

    Add TODOs for missing actions on characters

--- src/components/pages/character-page.vue
+++ src/components/pages/character-page.vue
@@ -5,6 +5,22 @@
     <p>({{ character.stars }} stars)</p>
 
     <p>You have <strong>{{ user.unspent_stars }}</strong> unspent stars.</p>
+
+    <h3>TODO:</h3>
+
+    <ul>
+      <li>Character selection dropdown</li>
+      <li>
+        Buttons after character name:
+        <ul>
+          <li>Add stars</li>
+          <li>Rename</li>
+          <li>Delete</li>
+        </ul>
+      </li>
+      <li>Level progress bar with current level</li>
+      <li>Banner display similar to the actual sheet</li>
+    </ul>
   </div>
   <p v-else>
     The character could not be found.
