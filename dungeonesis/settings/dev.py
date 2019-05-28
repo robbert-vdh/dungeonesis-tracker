fdc69ca11a8c2c1b6a32d9334c9bed31dc0d4516
Robbert van der Helm <mail@robbertvanderhelm.nl>
2019-05-28 22:53:22 +0200

    Add empty values for the OAuth tokens

--- dungeonesis/settings/dev.py
+++ dungeonesis/settings/dev.py
@@ -7,6 +7,10 @@ DEBUG = True
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "j%8-3s^_r7va1w8x1d_u=+f&tfgx($8@6_&m9!qqlsl@b+c&ty"
 
+# Don't forget to set these in local.py
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = None
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = None
+
 
 try:
     from .local import *
--- dungeonesis/settings/production.py
+++ dungeonesis/settings/production.py
@@ -3,6 +3,10 @@ from .base import *
 
 DEBUG = False
 
+# Don't forget to set these in local.py
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = None
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = None
+
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/ for an
 # overview of settings to set in local.py
 
